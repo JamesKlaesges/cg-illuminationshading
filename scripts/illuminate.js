c1f5142bb581105aae052d4392878e7f945153de
JamesKlaesges <50339142+JamesKlaesges@users.noreply.github.com>
2020-05-09 19:50:36 -0700

    trying to get shader to work

--- scripts/illuminate.js
+++ scripts/illuminate.js
@@ -123,15 +123,15 @@ class GlApp {
             this.gl.useProgram(this.shader[selected_shader].program);
 		
 	    //Pass data into the shader
-	    if (selected_shader == 'gouraud_color' || selected_shader == 'phong_color')
-	    {
+	    //if (selected_shader == 'gouraud_color' || selected_shader == 'phong_color')
+	    //{
 		    this.gl.uniform3fv(this.shader[selected_shader].light_ambient, this.scene.light.ambient)
 		    this.gl.uniform3fv(this.shader[selected_shader].light_position, this.scene.light.point_lights[0].position)
 		    this.gl.uniform3fv(this.shader[selected_shader].light_color, this.scene.light.point_lights[0].color)
 		    this.gl.uniform3fv(this.shader[selected_shader].camera_position, this.scene.camera.position)
 		    this.gl.uniform1f(this.material_shininess, this.scene.models[i].material.shininess);
 		    this.gl.uniform3fv(this.shader[selected_shader].uniform.material_specular, this.scene.models[i].material.specular);
-	    }
+	    //}
 		
             // transform model to proper position, size, and orientation
             glMatrix.mat4.identity(this.model_matrix);
