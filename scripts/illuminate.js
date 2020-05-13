7ef65c08f194b5854048f5bf3317b29bb284a690
JamesKlaesges <50339142+JamesKlaesges@users.noreply.github.com>
2020-05-13 13:33:23 -0700

    Update illuminate.js

--- scripts/illuminate.js
+++ scripts/illuminate.js
@@ -158,7 +158,7 @@ class GlApp {
             	this.gl.uniform3fv(this.shader[selected_shader].uniform.light_position[j], this.scene.light.point_lights[j].position);
             	this.gl.uniform3fv(this.shader[selected_shader].uniform.light_color[j], this.scene.light.point_lights[j].color);
 	    }
-	    this.gl.uniform3fv(this.shader[selected_shader].uniform.camera_position, this.scene.camera.position)
+	    this.gl.uniform3fv(this.shader[selected_shader].uniform.camera_position, this.scene.camera.position);
             this.gl.uniform1f(this.shader[selected_shader].uniform.material_shininess, this.scene.models[i].material.shininess);
             this.gl.uniform3fv(this.shader[selected_shader].uniform.material_specular, this.scene.models[i].material.specular);
             this.gl.uniform3fv(this.shader[selected_shader].uniform.material_color, this.scene.models[i].material.color);
